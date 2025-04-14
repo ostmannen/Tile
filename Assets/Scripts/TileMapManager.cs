f309ad019c76f3f4109fde65bec99a9467a0cdc0
ostmannen <60339614+ostmannen@users.noreply.github.com>
2025-04-14 16:45:32 +0200

    Initial commit

---
+++ Assets/Scripts/TileMapManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapManager : MonoBehaviour
+{
+    public Tilemaps tilemap;
+    public Tilemaps tilemaps;
+    public Grid grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
